5a59261fa414cccba819a2760fdfc056d1527110
gpfqk <gpfqk@naver.com>
2017-06-12 18:51:55 +0900

    추천예약 알고리즘만듬
    알고리즘만 만듬..

--- db.php
+++ db.php
@@ -230,9 +230,7 @@
 	{
 		$connect = mysql_connect("203.252.182.152", "all", "apmsetup");
 		$db = mysql_select_db("mysheet", $connect);
-		$query = "select * from studylist A, userinfo B ";
-		$query.= "where A.host = B.id ";
-		$query.= "and (host = '".$_SESSION['id']."' or member1 = '".$_SESSION['id']."' or member2 = '".$_SESSION['id']."' or member3 = '".$_SESSION['id']."');";
+		$query = "select sd_time from schedule where sd_time Like 'tue%';";
 		$result = mysql_query($query);
 
 		if($result)
@@ -240,16 +238,30 @@
 			for($i=0;$i<mysql_num_rows($result);$i++){
 		      	$row = mysql_fetch_array($result); 
 		      	$arr[$i] = $row['sd_time'];
-		      	echo $row['sd_time'];
 			}
 		}
 
 		for($i=0; $i<count($arr); $i++){
 			$result_array[$i] = explode(',',$arr[$i]);
-			echo "<br>".$result_array[$i][0]."//".$result_array[$i][1]."//".$result_array[$i][2]."//".$result_array[$i][3];
 		}
-		// print_r($result_array);
-		$query = "select num from schedule where sd_time not in 'tue%';";
-		echo "<br>".$result_array[0][0]."//".$result_array[0][1]."//".$result_array[0][2];
+/////////////////////여기까지 일정 스플릿해서 배열에 저장
+
+		$query = "select * from checklist where num not in (";
+		for($i=0;$i<count($result_array);$i++)
+			{
+				$query.= $result_array[$i][1].",".$result_array[$i][2].",".$result_array[$i][3];
+				if($i<count($result_array)-1)
+					$query.=",";
+			};
+			$query.=");";
+		$result = mysql_query($query);
+		if($result)
+	    {
+			for($i=0;$i<mysql_num_rows($result);$i++){
+		      	$row = mysql_fetch_array($result); 
+		      	$check_arr[$i] = $row['num'];
+		      	echo $check_arr[$i]."<br>";
+			}
+		}
 	}
 ?>
